0ef59d6e2110790890464a7a098df8a48ef37e75
Tran, Wilson <wdtran2018@gmail.com>
2026-03-15 23:55:20 -0700

    Created CoinFlip() function and initialized main program

--- Function_1.cpp
+++ Function_1.cpp
@@ -3,13 +3,37 @@
 using namespace std;
 
 /* Define your function here */ 
+string CoinFlip()
+{
+   if(rand() % 2 == 1)
+   {
+      return "Heads";
+   }
+   else
+   {
+      return "Tails";
+   }
+}
 
 int main() {
    // Add more variables as needed
+   int numFlips;
    
    srand(2);  // Unique seed
 
    /* Type your code here */
+   cout << "Enter the number of times to flip a coin: ";
+   cin >> numFlips;
+
+   for(int i = 0; i < numFlips; i++)
+   {
+      if(i > 0)
+      {
+         cout << " ";
+      }
+      cout << CoinFlip();
+   }
+   cout << endl;
 
    return 0;
 }
