39cdd08f61dc8f407e535bac4f1490e079654859
Tran, Wilson <wdtran2018@gmail.com>
2026-03-16 02:05:16 -0700

    Wrote a program that tells the user how much change to give out for an amount between 1-99. In addition to computeCoins(), I added two more helper functions

--- Function_4.cpp
+++ Function_4.cpp
@@ -2,12 +2,75 @@
 using namespace std;
 
 //define necessary functions
+bool getInput(int& amount);
+void computeCoins(int coinValue, int& num, int& amountLeft);
+void output(int amount);
+
+
 int main()
 {
+  int amount;
+  bool keepLooping;
 
-  //your code goes here
+  cout << "---------- Coin Change Calculator ----------" << endl;
 
-return 0;
+  keepLooping = getInput(amount);
+
+  while(keepLooping)
+  {
+    if(amount >= 1 && amount <= 99)
+      output(amount);
+    
+    keepLooping = getInput(amount);
+  }
+
+  cout << "Stopped." << endl;
+
+  return 0;
 }
 
 //implement functions
+bool getInput(int& amount)
+{
+  cout << "Enter an amount in cents (1-99), or -1 to quit): ";
+  cin >> amount;
+
+  if(amount == -1)
+    return false;
+
+  if(amount < -1 || amount > 99)
+  {
+    cout << "Invalid input. Please enter an integer between 1-99, or -1 to quit: ";
+    return true;
+  }
+
+  return true;
+}
+
+void computeCoins(int coinValue, int& num, int& amountLeft)
+{
+  num = amountLeft / coinValue;
+  amountLeft = amountLeft % coinValue;
+}
+
+
+void output(int amount)
+{
+  int quarters, dimes, pennies;
+  int amountLeft = amount;
+
+  computeCoins(25, quarters, amountLeft);
+  computeCoins(10, dimes, amountLeft);
+  computeCoins(1, pennies, amountLeft);
+
+  cout << amount << " cents can be given as: ";
+
+  if(quarters > 0)
+    cout << quarters << " quarter(s) ";
+  if(dimes > 0)
+    cout << dimes << " dime(s) ";
+  if(pennies > 0)
+    cout << pennies << " penny(pennies)";
+
+  cout << endl << endl;
+}
